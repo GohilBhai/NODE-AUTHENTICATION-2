b8eda710427c5afce9bfad68bae282212ee1ceeb
Gohil Ranjit <157449569+GohilBhai@users.noreply.github.com>
2024-05-02 17:36:22 +0530

    Update config.js

--- Authentication-master/config/config.js
+++ Authentication-master/config/config.js
@@ -1,11 +1,1 @@
-const secretSession = "sessionsecretofme";
 
-const emailUser = "rdgohil2002@gmail.com";
-const emailPassword = "silfkeqrfemakdgz";  // password ==> "silfkeqrfemakdgz"
-
-
-module.exports = {
-  secretSession,
-  emailUser,
-  emailPassword,
-};
